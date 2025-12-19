7edcd776b43815e479206cf8c46bd0f2f3ab226e
Ascelefeu <cuinet.adrien@orange.fr>
2025-12-19 13:20:51 +0100

    Ignore Rust target directory

--- gwynt/target/debug/deps/gwynt-c3d05be7e0d30fcd.d
+++
@@ -1,5 +0,0 @@
-C:\Users\cuine\Documents\Année 4\Rust\Projet\rust_project\gwynt\target\debug\deps\gwynt-c3d05be7e0d30fcd.d: src\main.rs
-
-C:\Users\cuine\Documents\Année 4\Rust\Projet\rust_project\gwynt\target\debug\deps\libgwynt-c3d05be7e0d30fcd.rmeta: src\main.rs
-
-src\main.rs:
